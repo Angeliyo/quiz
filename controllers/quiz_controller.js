b27b5dbcc26e9904bfe05d045d18451d3804ba13
Angeliyo <angel.plaza@gmail.com>
2015-07-23 18:57:46 +0200

    Correcciones y mejoras
    Correccion de versiones y mejoras propuestas por los compañeros que
    corrigieron la práctica

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,7 +2,7 @@ var models = require('../models/models.js');
 
 // Autoload - factoriza el código si ruta incluye :quizId
 exports.load = function(req, res, next, quizId) {
-  models.Quiz.findById(quizId).then(
+  models.Quiz.find(quizId).then(
     function(quiz) {
       if(quiz){
         req.quiz = quiz;
@@ -16,11 +16,24 @@ exports.load = function(req, res, next, quizId) {
 
 //GET /quizes
 exports.index = function(req,res){
-  models.Quiz.findAll().then(
-    function(quizes){
-      res.render('quizes/index.ejs', {quizes:quizes});
-    }
-  ).catch(function(error) {next(error);})
+  if(req.query.search){
+    var searchText = req.query.search;
+    // Se susituyen los espacios por comodines
+    searchText = searchText.replace(/ /g,'%');
+    // Se ponen comodines por delante y por detrás del texto de búsqueda
+    searchText = "%"+searchText+"%";
+    models.Quiz.findAll({where: ["pregunta like ?", searchText]}).then(
+      function(quizes){
+        res.render('quizes/index.ejs', {quizes:quizes, filtered:true});
+      }
+    ).catch(function(error) {next(error);});
+  }else{
+    models.Quiz.findAll().then(
+      function(quizes){
+        res.render('quizes/index.ejs', {quizes:quizes, filtered:false});
+      }
+    ).catch(function(error) {next(error);});
+  }
 };
 
 //GET /quizes/question
