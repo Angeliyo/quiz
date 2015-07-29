4b63c57b792457319f4e3d9e8f673316c3a95c9c
Angeliyo <angel.plaza@gmail.com>
2015-07-29 19:50:12 +0200

    Autenticación y Sesión

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz', errors:{}});
@@ -10,6 +12,11 @@ router.get('/', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load); //autoload :quizId
+
+// Definición de rutas de session
+router.get('/login', sessionController.new); //formulario login
+router.post('/login',sessionController.create);//crear sesión
+router.get('/logout',sessionController.destroy);//destruir sesión
 
 // Definición de rutas de /quizes
 router.get('/quizes', quizController.index);
