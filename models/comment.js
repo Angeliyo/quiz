96ba55865cb22b943b4f1ac6b798200b33df763e
Angeliyo <angel.plaza@gmail.com>
2015-07-30 20:05:04 +0200

    Moderación de comentarios

--- models/comment.js
+++ models/comment.js
@@ -5,6 +5,10 @@ return sequelize.define('Comment',
     { texto : {
         type: DataTypes.STRING,
         validate: { notEmpty: {msg: "-> Falta Comentario"}}
+      },
+      publicado: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
       }
     });
 }
